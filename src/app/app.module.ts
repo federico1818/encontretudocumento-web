2fb79b47b81277979d4cb77a79ad09561ada72fa
Federico González <federico1818@gmail.com>
2020-09-12 19:37:32 -0300

    add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+import { MaterialModule } from './material/material.module'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
     declarations: [
@@ -11,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     ],
     imports: [
         BrowserModule,
+        MaterialModule,
         AppRoutingModule,
         BrowserAnimationsModule
     ],
