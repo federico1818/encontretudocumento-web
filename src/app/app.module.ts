3cf884664399c4f35c470236c54f99606007f7a2
Federico González <federico1818@gmail.com>
2020-09-12 20:34:26 -0300

    install layout cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 import { MaterialModule } from './material/material.module'
 import { AppRoutingModule } from './app-routing.module'
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component'
     imports: [
         BrowserModule,
         MaterialModule,
+        FlexLayoutModule,
         AppRoutingModule,
         BrowserAnimationsModule
     ],
