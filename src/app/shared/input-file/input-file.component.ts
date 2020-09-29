c902ea54e2519a0b8373bea4a37cbaee304fa21d
Federico González <federico1818@gmail.com>
2020-09-29 00:39:23 -0300

    show image after change

--- src/app/shared/input-file/input-file.component.ts
+++ src/app/shared/input-file/input-file.component.ts
@@ -7,18 +7,31 @@ import { Component, OnInit } from '@angular/core'
 })
 
 export class InputFileComponent implements OnInit {
+    public imgSrc
 
-    constructor() { }
+    constructor() {}
 
     ngOnInit(): void {
     }
 
     public onchange(event: Event): void {
         if(this.hasFile(event))
-            console.log('Lalala')
+            this.renderFile(event)
+    }
+
+    protected renderFile(event: Event) {
+        let fileReader = new FileReader()
+        fileReader.onloadend = () => {
+            this.imgSrc = fileReader.result
+        }
+        fileReader.readAsDataURL(event.target['files'][0])
     }
 
     protected hasFile(event: Event) {
         return event.target['files'] && event.target['files'][0]
     }
+
+    public get imageUploaded(): boolean {
+        return this.imgSrc
+    } 
 }
