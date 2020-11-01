0e325fec76fbff472cf75d7d1e706c869f76039c
Federico González <federico1818@gmail.com>
2020-11-01 01:32:16 -0300

    document request message

--- src/app/document/pages/document-request-create/document-request-create.component.ts
+++ src/app/document/pages/document-request-create/document-request-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core'
+import { FormBuilder, Validators } from '@angular/forms'
+import { ActivatedRoute } from '@angular/router'
+import { Post } from 'src/app/core/post'
+import { PostService } from 'src/app/post/services/post.service'
 
 @Component({
     selector: 'app-document-request-create',
@@ -7,10 +11,24 @@ import { Component, OnInit } from '@angular/core'
 })
 
 export class DocumentRequestCreateComponent implements OnInit {
-
-    constructor() { }
+    public post: Post
+    
+    public requestContactForm = this.fb.group({
+        name: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        message: ['', []]
+    })
+    
+    constructor(
+        protected route: ActivatedRoute,
+        protected fb: FormBuilder,
+        protected postService: PostService
+    ) {}
 
     ngOnInit(): void {
+        this.route.params.subscribe(params => {
+            this.post = this.postService.find(params['id'])
+        })
     }
 
 }
